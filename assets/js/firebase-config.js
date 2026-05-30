abc9f561cafeaf64e9f4164a91a2a82a8df056e3
Gabriel-Angel0 <gabriel.koats@gmail.com>
2026-05-30 00:32:37 -0300

    Add Firebase config template

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase config do projeto Alumni UFJF
+// 1. Crie um projeto em https://console.firebase.google.com/
+// 2. Ative Authentication com Email/Senha
+// 3. Ative Cloud Firestore
+// 4. Substitua os valores abaixo pelas credenciais do seu app Web
+
+export const firebaseConfig = {
+  apiKey: "COLE_SUA_API_KEY_AQUI",
+  authDomain: "COLE_SEU_AUTH_DOMAIN_AQUI",
+  projectId: "COLE_SEU_PROJECT_ID_AQUI",
+  storageBucket: "COLE_SEU_STORAGE_BUCKET_AQUI",
+  messagingSenderId: "COLE_SEU_MESSAGING_SENDER_ID_AQUI",
+  appId: "COLE_SEU_APP_ID_AQUI"
+};
