4ac92b0098ee6dd6c56029c13e07cf975c0a0482
Gabriel-Angel0 <gabriel.koats@gmail.com>
2026-05-30 00:52:49 -0300

    Configure Firebase project credentials

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,14 +1,9 @@
-// Firebase config do projeto Alumni UFJF
-// 1. Crie um projeto em https://console.firebase.google.com/
-// 2. Ative Authentication com Email/Senha
-// 3. Ative Cloud Firestore
-// 4. Substitua os valores abaixo pelas credenciais do seu app Web
-
 export const firebaseConfig = {
-  apiKey: "COLE_SUA_API_KEY_AQUI",
-  authDomain: "COLE_SEU_AUTH_DOMAIN_AQUI",
-  projectId: "COLE_SEU_PROJECT_ID_AQUI",
-  storageBucket: "COLE_SEU_STORAGE_BUCKET_AQUI",
-  messagingSenderId: "COLE_SEU_MESSAGING_SENDER_ID_AQUI",
-  appId: "COLE_SEU_APP_ID_AQUI"
+  apiKey: "AIzaSyCYojZ3PNZJLKQzelI2dzSiL1v5nKmYMuM",
+  authDomain: "alumni-ufjf.firebaseapp.com",
+  projectId: "alumni-ufjf",
+  storageBucket: "alumni-ufjf.firebasestorage.app",
+  messagingSenderId: "474955724590",
+  appId: "1:474955724590:web:94cd853a2db4a177fb2cbf",
+  measurementId: "G-99BE39HP2Y"
 };
